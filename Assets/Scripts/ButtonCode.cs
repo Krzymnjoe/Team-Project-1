a16bdc69e6065ff2c671ed66a38fb3b960ff188d
DaGamer327 <jaysmind123@gmail.com>
2021-02-17 16:55:25 -0500

    Tech

---
+++ Assets/Scripts/ButtonCode.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonCode : MonoBehaviour
+{
+    public Text WinLose;
+
+    public bool win;
+    public bool lose;
+
+    void Start()
+    {
+        WinLose.text = "";
+
+        win = false;
+        lose = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SceneManager.LoadScene(3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Time.timeScale = 0;
+            WinLose.text = "You Lose! \n \n Press R to Return to \n the Main Menu \n or Escape to Quit";
+            lose = true;
+
+            if (lose == true)
+            {
+                FindObjectOfType<MusicSwitch>().DMusic();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Time.timeScale = 0;
+            WinLose.text = "You Win! \n \n Press R to Restart to \n the Main Menu \n or Escape to Quit";
+            win = true;
+
+            if (win == true)
+            {
+                FindObjectOfType<MusicSwitch>().VMusic();
+            }
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+
+        }
+
+        if (Time.timeScale == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Time.timeScale = 1;
+                SceneManager.LoadScene(0);
+
+            }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+
+            }
+
+        }
+    }
+        public void QuitGame()
+        {
+            Application.Quit();
+            Debug.Log("Game is exiting");
+        }
+
+        public void NextLevelButton(int index)
+        {
+            SceneManager.LoadScene(index);
+        }
+
+        public void QuitGame(string Game)
+        {
+            Application.Quit();
+        }
+
+    
+}
---
+++ Assets/Scripts/MusicSwitch.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwitch : MonoBehaviour
+{
+
+    public AudioSource LevelMusic;
+    public AudioSource DeathMusic;
+    public AudioSource VictoryMusic;
+
+    public bool levelcheck;
+    public bool deathcheck;
+    public bool victorycheck;
+
+    //COPY LINES INTO THE PLAYER SCRIPT TO USE THIS ONE PROPERLY
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BGMusic()
+    {
+        levelcheck = true;
+        deathcheck = false;
+        victorycheck = false;
+        LevelMusic.Play();
+    }
+
+    public void DMusic()
+    {
+        if (LevelMusic.isPlaying)
+            levelcheck = false;
+        {
+            LevelMusic.Stop();
+        }
+        if (!DeathMusic.isPlaying && deathcheck == false)
+        {
+            DeathMusic.Play();
+            deathcheck = true;
+        }
+    }
+
+    public void VMusic()
+    {
+        if (LevelMusic.isPlaying)
+            levelcheck = false;
+        {
+            LevelMusic.Stop();
+        }
+        if (!VictoryMusic.isPlaying && victorycheck == false)
+        {
+            VictoryMusic.Play();
+            victorycheck = true;
+        }
+    }
+}
